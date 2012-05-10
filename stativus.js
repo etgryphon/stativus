6f87cd6487eb82337b81e717f2b31f67722d1816
Manuel Kreutz <manuel@140.am>
2012-05-10 13:40:09 -0400

    cleaner alternative to add ES5 forEach support to support IE 6.0+

--- stativus.js
+++ stativus.js
@@ -10,6 +10,36 @@ if (!Array.prototype.indexOf) {
   }
 }
 
+// Adds indexOf support for <IE8
+// https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference:Objects:Array:forEach#Compatibility
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.com/#x15.4.4.18
+if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function( callback, thisArg ) {
+    var T, k;
+    if ( this == null ) {
+      throw new TypeError( " this is null or not defined" );
+    }
+    var O = Object(this);
+    var len = O.length >>> 0; // Hack to convert O.length to a UInt32
+    if ( {}.toString.call(callback) != "[object Function]" ) {
+      throw new TypeError( callback + " is not a function" );
+    }
+    if ( thisArg ) {
+      T = thisArg;
+    }
+    k = 0;
+    while( k < len ) {
+      var kValue;
+      if ( k in O ) {
+        kValue = O[ k ];
+        callback.call( T, kValue, k, O );
+      }
+      k++;
+    }
+  };
+}
+
 /**
   This is the code for creating statecharts in your javascript files
   
