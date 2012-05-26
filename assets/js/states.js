2126dcc9c8765aa68f39bfdd03644bf0e6e58a87
Evin Grano <etgryphon@sproutcore.com>
2012-05-26 07:48:41 -0400

    [WIP] Added the state charts and a construction modal while I work on the page

---
+++ assets/js/states.js
@@ -0,0 +1,62 @@
+/*globals $ Stativus*/
+var statechart = Stativus.createStatechart();
+// *********************************
+// DEFAULT STATES
+// *********************************
+statechart.addState("#home", {
+  
+  enterState: function(){
+    $('.home-page').show('fast');
+  },
+  
+  exitState: function(){
+    $('.home-page').hide('fast');
+  }
+});
+
+// *********************************
+// MODAL STATES
+// *********************************
+statechart.addState("#modal_ready", {
+  globalConcurrentState: 'modal_states',
+  
+  // events
+  underConstruction: function(id){
+    this.goToState('#underConstruction');
+  }
+});
+
+statechart.addState("#underConstruction", {
+  globalConcurrentState: 'modal_states',
+  actions: {
+    '.close-action click': 'closeModal'
+  },
+  enterState: function(){
+    $('#info-modal .modal-header').html('<h2>Under Construction!</h2>');
+    $('#info-modal .modal-body').html('Page is under construction.  Anything with a \'*\' hasn\'t been completed');
+    $('#info-modal').modal('show');
+  },
+  exitState: function(){
+    $('#info-modal').modal('hide');
+  },
+  
+  // Events
+  closeModal: function(){
+    this.goToState('#modal_ready');
+  }
+});
+
+// *********************************
+// JQuery Startup functions
+// *********************************
+$(document).ready(function() {
+  // Handler for .ready() called.
+  statechart.initStates({'default': '#home', 'modal_states': '#underConstruction'});
+});
+
+$(document).bind('click', function(evt){
+  statechart.tryToPerform(evt);
+});
+this.statechart = statechart;
+
+
