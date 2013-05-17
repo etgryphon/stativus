502cb4abe915b161d2f724a754a946557689440f
Richard Harriman <richard.harriman@eloqua.com>
2013-05-17 12:25:42 -0400

    Removed globals from unit tests and cleaned up the tests

--- test/libs/test_init.js
+++ test/libs/test_init.js
@@ -1,164 +1,159 @@
 /*globals $ equal module expect myStatechart Stativus*/
-var SC, stateTransitions;
-var runInitTests = function(){
-  
-  module("Module: Test Initialization", {
-    setup: function(){
-      var tr = window.TestRunner;
-      SC = tr.createDefaultStatechart();
-    }
+
+module("Module: Test Initialization", {
+  setup: function(){
+    this.sc = window.TestRunner.createDefaultStatechart();
+  }
+});
+
+test("Is Statechart Initialization Correct?", function() {
+  var cStates, testNames = 'first,first.first.first,first.second.first';
+  expect(6);
+  cStates = this.sc.currentState();
+  equal( cStates.length, 5, "In the Default State: there are 5 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
   });
+});
+
+module("Module: Test most basic initializations", {
+  setup: function(){
+    this.sc = Stativus.createStatechart();
+    this.sc.addState('#application');
+    this.sc.initStates("#application");
+  }
+});
+
+test("Is Statechart Initialization Correct for single state w/ only a name?", function() {
+  var cStates, testNames = '#application';
+  expect(2);
+  cStates = this.sc.currentState();
+  equal( cStates.length, 1, "In the Default State: there are 1 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+  });
+});
   
-  test("Is Statechart Initialization Correct?", function() {
-    var cStates, testNames = 'first,first.first.first,first.second.first';
-    expect(6);
-    cStates = SC.currentState();
-    equal( cStates.length, 5, "In the Default State: there are 5 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+module("Module: Test Edge Initializations", {
+  setup: function(){
+    this.sc = Stativus.createStatechart();
+    this.sc.addState("#application", {
+      substatesAreConcurrent: true
     });
-  });
-  
-  module("Module: Test most basic initializations", {
-    setup: function(){
-      var sc = Stativus.createStatechart();
-      sc.addState('#application');
-      sc.initStates("#application");
-      SC = sc;
-    }
-  });
-  
-  test("Is Statechart Initialization Correct for single state w/ only a name?", function() {
-    var cStates, testNames = '#application';
-    expect(2);
-    cStates = SC.currentState();
-    equal( cStates.length, 1, "In the Default State: there are 1 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+    this.sc.addState("#subapplication", {
+      parentState: "#application",
+      substatesAreConcurrent: true
     });
+
+    this.sc.addState("#first", {
+      parentState: "#subapplication"
+    });
+
+    this.sc.addState("#second", {
+      parentState: "#subapplication"
+    });
+    this.sc.initStates("#application");
+  }
+});
+
+test("Is Statechart Initialization Correct for Single parallel concurrent substate?", function() {
+  var cStates, testNames = '#application,#subapplication,#first,#second';
+  expect(5);
+  cStates = this.sc.currentState();
+  equal( cStates.length, 4, "In the Default State: there are 4 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
   });
+});  
+
+module("Module: Test Nested Initializations", {
+  setup: function(){    
+    this.statecharts = [];  
+    // statechart with hashs as substates
+    var sc1 = Stativus.createStatechart();
+    sc1.addState("#application", {
+      initialSubstate: '#first',
+      states: [ 
+        { name: '#first'},
+        { name: '#second'}
+      ]
+    });
+
+    sc1.initStates("#application");
+    this.statecharts.push(sc1);
     
-  module("Module: Test Edge Initializations", {
-    setup: function(){
-      var sc = Stativus.createStatechart();
-      sc.addState("#application", {
-        substatesAreConcurrent: true
-      });
-      sc.addState("#subapplication", {
-        parentState: "#application",
-        substatesAreConcurrent: true
-      });
-  
-      sc.addState("#first", {
-        parentState: "#subapplication"
-      });
-  
-      sc.addState("#second", {
-        parentState: "#subapplication"
-      });
-      sc.initStates("#application");
-      SC = sc;
-    }
-  });
-  
-  test("Is Statechart Initialization Correct for Single parallel concurrent substate?", function() {
-    var cStates, testNames = '#application,#subapplication,#first,#second';
-    expect(5);
-    cStates = SC.currentState();
-    equal( cStates.length, 4, "In the Default State: there are 4 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+    // statechart with arrays as substates
+    var sameCode = {
+      testEvent: function(){
+        this.goToState({'#first': '#second', '#second': '#first'}[this.name]);
+      }
+    };
+    var sc2 = Stativus.createStatechart();
+    sc2.addState("#application", {
+      initialSubstate: '#first',
+      states: [ 
+        ['#first', sameCode],
+        ['#second', sameCode]
+      ]
     });
-  });  
+    sc2.initStates("#application");
+    this.statecharts.push(sc2);
+  }
+});
   
-  module("Module: Test Nested Initializations", {
-    setup: function(){    
-      SC = [];  
-      // statechart with hashs as substates
-      var sc1 = Stativus.createStatechart();
-      sc1.addState("#application", {
-        initialSubstate: '#first',
-        states: [ 
-          { name: '#first'},
-          { name: '#second'}
-        ]
-      });
-      sc1.initStates("#application");
-      SC.push(sc1);
-      
-      // statechart with arrays as substates
-      var sameCode = {
-        testEvent: function(){
-          this.goToState({'#first': '#second', '#second': '#first'}[this.name]);
-        }
-      };
-      var sc2 = Stativus.createStatechart();
-      sc2.addState("#application", {
-        initialSubstate: '#first',
-        states: [ 
-          ['#first', sameCode],
-          ['#second', sameCode]
-        ]
-      });
-      sc2.initStates("#application");
-      SC.push(sc2);
-    }
+test("Test the state with object substates?", function() {
+  var cStates, sc = this.statecharts[0], testNames = '#application,#first';
+  cStates = sc.currentState();
+  equal( cStates.length, 2, "In the Default State: there are 2 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
   });
-    
-  test("Test the state with object substates?", function() {
-    var cStates, sc = SC[0], testNames = '#application,#first';
-    cStates = sc.currentState();
-    equal( cStates.length, 2, "In the Default State: there are 2 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+});
+
+test("Test the state with array substates?", function() {
+  var cStates, sc = this.statecharts[1], testNames = '#application,#first', testNames2 = '#application,#second';
+  cStates = sc.currentState();
+  equal( cStates.length, 2, "In the Default State: there are 2 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
+  });
+  sc.sendEvent('testEvent');
+  equal( cStates.length, 2, "In the Transitioned State: there are 2 current states" );
+  cStates = sc.currentState();
+  cStates.forEach( function(x){
+    ok( testNames2.indexOf(x.name) > -1, "In the Transitioned State: there is current state named: "+x.name );
+  });
+});
+
+module("Module: Test Advanced Nested Initializations", {
+  setup: function(){      
+    // statechart with hashs as substates
+    this.sc = Stativus.createStatechart();
+    this.sc.addState("#application", {
+      initialSubstate: '#first',
+      states: [ 
+        { 
+          name: '#first',
+          initialSubstate: '#first.first',
+          states: [
+            { name: '#first.first'},
+            { name: '#first.second'}
+          ]
+        },
+        { name: '#second'}
+      ]
     });
+    this.sc.initStates("#application");
+  }
+});
+
+test("Test the state with advanced object substates?", function() {
+  var cStates, testNames = '#application,#first,#first.first';
+  expect(4);
+  cStates = this.sc.currentState();
+  equal( cStates.length, 3, "In the Default State: there are 3 current states" );
+  cStates.forEach( function(x){
+    ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
   });
-  
-  test("Test the state with array substates?", function() {
-    var cStates, sc = SC[1], testNames = '#application,#first', testNames2 = '#application,#second';
-    cStates = sc.currentState();
-    equal( cStates.length, 2, "In the Default State: there are 2 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
-    });
-    sc.sendEvent('testEvent');
-    equal( cStates.length, 2, "In the Transitioned State: there are 2 current states" );
-    cStates = sc.currentState();
-    cStates.forEach( function(x){
-      ok( testNames2.indexOf(x.name) > -1, "In the Transitioned State: there is current state named: "+x.name );
-    });
-  });
-  
-  module("Module: Test Advanced Nested Initializations", {
-    setup: function(){      
-      // statechart with hashs as substates
-      SC = Stativus.createStatechart();
-      SC.addState("#application", {
-        initialSubstate: '#first',
-        states: [ 
-          { 
-            name: '#first',
-            initialSubstate: '#first.first',
-            states: [
-              { name: '#first.first'},
-              { name: '#first.second'}
-            ]
-          },
-          { name: '#second'}
-        ]
-      });
-      SC.initStates("#application");
-    }
-  });
-  
-  test("Test the state with advanced object substates?", function() {
-    var cStates, testNames = '#application,#first,#first.first';
-    expect(4);
-    cStates = SC.currentState();
-    equal( cStates.length, 3, "In the Default State: there are 3 current states" );
-    cStates.forEach( function(x){
-      ok( testNames.indexOf(x.name) > -1, "In the Default State: there is current state named: "+x.name );
-    });
-  });
-  
-};
+});
+
--- test/libs/test_statechart_testing.js
+++ test/libs/test_statechart_testing.js
@@ -1,125 +1,124 @@
 /*globals $ equal module expect myStatechart Stativus start stop*/
-var SC, stateTransitions, mockObject;
-var runStatechartTestingTests = function(){
+
+module("Test Statechart Testing", {
+  setup: function(){
+    mockObject = {
+      willEnterCount: 0,
+      enterCount: 0,
+      exitCount: 0,
+      willExitCount: 0
+    };
     
-  module("Module: Test Statechart Testing", {
-    setup: function(){
-      mockObject = {
-        willEnterCount: 0,
-        enterCount: 0,
-        exitCount: 0,
-        willExitCount: 0
-      };
-      var sc = Stativus.createStatechart();
-      sc.addState("#application", {
-        initialSubstate: '#subapplication',
-        enterState: function(){
-          console.log("Enter state...");
-          mockObject.enterCount = mockObject.enterCount+1;
-        },
-        exitState: function(){
-          console.log('blah...');
-          mockObject.exitCount = mockObject.exitCount+1;
-        },
-        testEvent: function(){
-          console.log('Test Event Fired...');
-        },
-        testEventHandled: function(){
-          console.log('Test Event Fired...');
-          return true;
-        }
-      });
-      sc.addState("#subapplication1", {
-        parentState: "#application",
-        transitionEvent: function(){
-          this.goToState('#subapplication2');
-        },
-        cascadeEvent: function(){
-          sc.sendEvent('transitionEvent');
-        }
-      });
-      sc.addState("#subapplication2", {
-        parentState: "#application",
-        enterState: function(){
-          this.goToState('#subapplication1');
-        }
-      });
-      sc.addState("#async", {
-        willEnterState: function(done){
-          var foo = function(){
-            mockObject.willEnterCount = mockObject.willEnterCount+1;
-            done();
-          };
-          window.setTimeout(foo, 2000);
-          return true;
-        },
-        willExitState: function(done){
-          var foo = function(){
-            mockObject.willExitCount = mockObject.willExitCount+1;
-            done();
-          };
-          window.setTimeout(foo, 2000);
-          return true;
-        }
-      });
-      SC = sc;
-    }
-  });
-  
-  test("Is TestStatechart Created?", function() {
-    var state = SC.loadState('#application');
-    ok( SC.isTestingStatechart, 'Testing Statechart was created' );
-  });
-  
-  test("Check to see if basic state tests work", function(){
-    var state = SC.loadState("#application");
-    equal(mockObject.enterCount, 0, 'mockObject.enterCount is 0');
-    equal(mockObject.exitCount, 0, 'mockObject.exitCount is 0');
-    state.enterState();
-    equal(mockObject.enterCount, 1, 'enterState was successfully called');
-    SC.sendEvent('testEvent');
-    ok(state.wasEvent('testEvent').called(), 'testEvent was called and handled on #application');
-    ok(!state.wasEvent('testEvent').handled(),'testEvent will continue to propagate to parent');
-    SC.sendEvent('testEventHandled');
-    ok(state.wasEvent('testEventHandled').called(), 'testEventHandled was called and handled on #application');
-    ok(state.wasEvent('testEventHandled').handled(),'testEventHandled was handled, no propagation to parent');
-    state.exitState();
-    equal(mockObject.exitCount, 1, 'exitState was successfully called');
-  });
-  
-  test("Check to see if transition tests work", function(){
-    var state = SC.loadState("#subapplication1");
-    SC.sendEvent('transitionEvent');
-    ok(state.wasEvent('transitionEvent').called(), 'transitionEvent was called and handled on #subapplication1');
-    ok(state.wasEvent('transitionEvent').transitionedTo('#subapplication2'), 'transitionEvent called transitioned to #subapplication2');
-    SC.sendEvent('cascadeEvent');
-    ok(state.wasEvent('cascadeEvent').called(), 'cascadeEvent was called and handled on #subapplication1');
-    ok(state.wasEvent('transitionEvent').called(2), 'cascadeEvent triggers a 2nd transitionEvent on #subapplication1');
-  });
-  
-  test("Check to see if switch states work", function(){
-    var state = SC.loadState("#subapplication2");
-    state.enterState();
-    ok(state.transitionedTo('#subapplication1'), 'switch state had proper transition');
-  });
-  
-  test("Check to see if async enter calls work", function(){
-    var state = SC.loadState("#async");
-    state.willEnterState(function(){
-      start();
-      ok(state.willEnterCompleted(), 'Will enter async call was completed');
-      equal(mockObject.willEnterCount, 1, 'willEnterCount was successfully called');
+    this.sc = Stativus.createStatechart();
+    this.sc.addState("#application", {
+      initialSubstate: '#subapplication',
+      enterState: function(){
+        console.log("Enter state...");
+        mockObject.enterCount = mockObject.enterCount+1;
+      },
+      exitState: function(){
+        console.log('blah...');
+        mockObject.exitCount = mockObject.exitCount+1;
+      },
+      testEvent: function(){
+        console.log('Test Event Fired...');
+      },
+      testEventHandled: function(){
+        console.log('Test Event Fired...');
+        return true;
+      }
     });
-    stop();
-  });
-  
-  test("Check to see if async exit calls work", function(){
-    var state = SC.loadState("#async");
-    state.willExitState(function(){
-      start();
-      ok(state.willExitCompleted(), 'Will exit async call was completed');
-      equal(mockObject.willExitCount, 1, 'willExitCount was successfully called');
+
+    this.sc.addState("#subapplication1", {
+      parentState: "#application",
+      transitionEvent: function(){
+        this.goToState('#subapplication2');
+      },
+      cascadeEvent: function(){
+        this.sendEvent('transitionEvent');
+      }
     });
-    stop();
+
+    this.sc.addState("#subapplication2", {
+      parentState: "#application",
+      enterState: function(){
+        this.goToState('#subapplication1');
+      }
+    });
+
+    this.sc.addState("#async", {
+      willEnterState: function(done){
+        var foo = function(){
+          mockObject.willEnterCount = mockObject.willEnterCount+1;
+          done();
+        };
+        window.setTimeout(foo, 2000);
+        return true;
+      },
+      willExitState: function(done){
+        var foo = function(){
+          mockObject.willExitCount = mockObject.willExitCount+1;
+          done();
+        };
+        window.setTimeout(foo, 2000);
+        return true;
+      }
+    });
+  }
+});
+
+test("Is TestStatechart Created?", function() {
+  var state = this.sc.loadState('#application');
+  ok( this.sc.isTestingStatechart, 'Testing Statechart was created' );
+});
+
+test("Check to see if basic state tests work", function(){
+  var state = this.sc.loadState("#application");
+  equal(mockObject.enterCount, 0, 'mockObject.enterCount is 0');
+  equal(mockObject.exitCount, 0, 'mockObject.exitCount is 0');
+  state.enterState();
+  equal(mockObject.enterCount, 1, 'enterState was successfully called');
+  this.sc.sendEvent('testEvent');
+  ok(state.wasEvent('testEvent').called(), 'testEvent was called and handled on #application');
+  ok(!state.wasEvent('testEvent').handled(),'testEvent will continue to propagate to parent');
+  this.sc.sendEvent('testEventHandled');
+  ok(state.wasEvent('testEventHandled').called(), 'testEventHandled was called and handled on #application');
+  ok(state.wasEvent('testEventHandled').handled(),'testEventHandled was handled, no propagation to parent');
+  state.exitState();
+  equal(mockObject.exitCount, 1, 'exitState was successfully called');
+});
+
+test("Check to see if transition tests work", function(){
+  var state = this.sc.loadState("#subapplication1");
+  this.sc.sendEvent('transitionEvent');
+  ok(state.wasEvent('transitionEvent').called(), 'transitionEvent was called and handled on #subapplication1');
+  ok(state.wasEvent('transitionEvent').transitionedTo('#subapplication2'), 'transitionEvent called transitioned to #subapplication2');
+  this.sc.sendEvent('cascadeEvent');
+  ok(state.wasEvent('cascadeEvent').called(), 'cascadeEvent was called and handled on #subapplication1');
+  ok(state.wasEvent('transitionEvent').called(2), 'cascadeEvent triggers a 2nd transitionEvent on #subapplication1');
+});
+
+test("Check to see if switch states work", function(){
+  var state = this.sc.loadState("#subapplication2");
+  state.enterState();
+  ok(state.transitionedTo('#subapplication1'), 'switch state had proper transition');
+});
+
+asyncTest("Check to see if async enter calls work", function(){
+  var state = this.sc.loadState("#async");
+  state.willEnterState(function(){
+    start();
+    ok(state.willEnterCompleted(), 'Will enter async call was completed');
+    equal(mockObject.willEnterCount, 1, 'willEnterCount was successfully called');
   });
-};
+});
+
+asyncTest("Check to see if async exit calls work", function(){
+  var state = this.sc.loadState("#async");
+  state.willExitState(function(){
+    start();
+    ok(state.willExitCompleted(), 'Will exit async call was completed');
+    equal(mockObject.willExitCount, 1, 'willExitCount was successfully called');
+  });
+});
+
